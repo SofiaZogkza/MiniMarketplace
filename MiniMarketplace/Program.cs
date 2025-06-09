f9c1d14ff576103ddefb467f964ba088656b6556
SofiaZogkza <sofiazogkza@Mac.fritz.box>
2025-06-09 20:23:45 +0200

    Added a space

--- MiniMarketplace/Program.cs
+++ MiniMarketplace/Program.cs
@@ -14,6 +14,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
